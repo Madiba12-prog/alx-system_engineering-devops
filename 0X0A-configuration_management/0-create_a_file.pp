37b8c72c18a597bff814ce3e80ef016bca916c3d
Madi <57211874+Madiba12-prog@users.noreply.github.com>
2023-03-24 13:01:37 +0300

    Task 0

--- 0X0A-configuration_management/0-create_a_file.pp
+++ 0X0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Using Puppet, create a file in /tmp
 file { '/tmp/school':
   ensure  => 'present',
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
